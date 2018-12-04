8c0ef322c1841085ddbe65e2530eb45d312614c0
truongnnd00546 <truongnnd00546@fpt.edu.vn>
2018-12-04 10:39:13 +0700

    nop bai thi

---
+++ Context/ProductContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using WebApplicationDevelopment.Models;
+
+namespace WebApplicationDevelopment.Context
+{
+    public class ProductContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplicationDevelopment.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name = "Exam Subject")]
+        public string Subject { get; set; }
+
+        [Display(Name = "Start Time")]
+        [Required(ErrorMessage = "Không được bỏ trống ô này")]
+        [DataType(DataType.Time)]
+        public DateTime StartTime { get; set; }
+
+        [Display(Name = "Exam Date")]
+        [Required(ErrorMessage = "Không được bỏ trống ô này")]
+        [DataType(DataType.Date)]
+        public DateTime ExamDate { get; set; }
+
+        [Display(Name = "Exam Duration")]
+        [Required(ErrorMessage = "Không được bỏ trống ô này")]
+        public int Duration { get; set; }
+
+        [Display(Name = "Class Room")]
+        public string ClassRoom { get; set; }
+
+        [Display(Name = "Faculty")]
+        public string Faculty { get; set; }
+
+    }
+}
